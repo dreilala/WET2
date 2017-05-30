dc9542bbc6a7c6561291e410773b605c5202ea88
Dreilala <brummbaerig@gmail.com>
2017-05-30 23:00:50 +0200

    template

--- subpages/products.php
+++ subpages/products.php
@@ -13,56 +13,235 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
   </head>
   <body>
-    <?php
-//    while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-//        extract($row);
 
-        // creating box
-        echo "<div class='col-md-4 m-b-20px'>";
+      <!-- Navigation -->
+      <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+          <div class="container">
+              <!-- Brand and toggle get grouped for better mobile display -->
+              <div class="navbar-header">
+                  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                      <span class="sr-only">Toggle navigation</span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                  </button>
+                  <a class="navbar-brand" href="#">Start Bootstrap</a>
+              </div>
+              <!-- Collect the nav links, forms, and other content for toggling -->
+              <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                  <ul class="nav navbar-nav">
+                      <li>
+                          <a href="#">About</a>
+                      </li>
+                      <li>
+                          <a href="#">Services</a>
+                      </li>
+                      <li>
+                          <a href="#">Contact</a>
+                      </li>
+                  </ul>
+              </div>
+              <!-- /.navbar-collapse -->
+          </div>
+          <!-- /.container -->
+      </nav>
 
-            // product id for javascript access
-            echo "<div class='product-id display-none'>ID</div>";
+      <!-- Page Content -->
+      <div class="container">
 
-            echo "<a href='product.php?id=Id' class='product-link'>";
-                // select and show first product image
-//                $product_image->product_id=$id;
-    /*            $stmt_product_image=$product_image->readFirst();
+          <div class="row">
 
-                while ($row_product_image = $stmt_product_image->fetch(PDO::FETCH_ASSOC)){
-                    echo "<div class='m-b-10px'>";
-                        echo "<img src='uploads/images/{$row_product_image['name']}' class='w-100-pct' />";
-                    echo "</div>";
-                } */
+              <div class="col-md-3">
+                  <p class="lead">Shop Name</p>
+                  <div class="list-group">
+                      <a href="#" class="list-group-item">Category 1</a>
+                      <a href="#" class="list-group-item">Category 2</a>
+                      <a href="#" class="list-group-item">Category 3</a>
+                  </div>
+              </div>
 
-                // product name
-                echo "<div class='product-name m-b-10px'>NAME</div>";
-            echo "</a>";
+              <div class="col-md-9">
 
-            // product price and category name
-                echo "<div class='m-b-10px'>";
-                    echo "&#36;" . number_format(122, 2, '.', ',');
-                echo "</div>";
+                  <div class="row carousel-holder">
 
-                // add to cart button
-                echo "<div class='m-b-10px'>";
-                    // cart item settings
-    //                $cart_item->user_id=1; // we default to a user with ID "1" for now
-    //                $cart_item->product_id=1;
+                      <div class="col-md-12">
+                          <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                              <ol class="carousel-indicators">
+                                  <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                                  <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                                  <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                              </ol>
+                              <div class="carousel-inner">
+                                  <div class="item active">
+                                      <img class="slide-image" src="http://placehold.it/800x300" alt="">
+                                  </div>
+                                  <div class="item">
+                                      <img class="slide-image" src="http://placehold.it/800x300" alt="">
+                                  </div>
+                                  <div class="item">
+                                      <img class="slide-image" src="http://placehold.it/800x300" alt="">
+                                  </div>
+                              </div>
+                              <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+                                  <span class="glyphicon glyphicon-chevron-left"></span>
+                              </a>
+                              <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+                                  <span class="glyphicon glyphicon-chevron-right"></span>
+                              </a>
+                          </div>
+                      </div>
 
-                    // if product was already added in the cart
-//                    if($cart_item->exists()){
-//                        echo "<a href='cart.php' class='btn btn-success w-100-pct'>";
-//                            echo "Update Cart";
-//                        echo "</a>";
-//                    }else{
-                        echo "<a href='add_to_cart.php?id=1&page={1}' class='btn btn-primary w-100-pct'>Add to Cart</a>";
-//                    }
-                echo "</div>";
+                  </div>
 
+                  <div class="row">
 
+                      <div class="col-sm-4 col-lg-4 col-md-4">
+                          <div class="thumbnail">
+                              <img src="http://placehold.it/320x150" alt="">
+                              <div class="caption">
+                                  <h4 class="pull-right">$24.99</h4>
+                                  <h4><a href="#">First Product</a>
+                                  </h4>
+                                  <p>See more snippets like this online store item at <a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>
+                              </div>
+                              <div class="ratings">
+                                  <p class="pull-right">15 reviews</p>
+                                  <p>
+                                      <span class="glyphicon glyphicon-star"></span>
+                                      <span class="glyphicon glyphicon-star"></span>
+                                      <span class="glyphicon glyphicon-star"></span>
+                                      <span class="glyphicon glyphicon-star"></span>
+                                      <span class="glyphicon glyphicon-star"></span>
+                                  </p>
+                              </div>
+                          </div>
+                      </div>
 
-        echo "</div>";
-//    }
-    ?>
+                      <div class="col-sm-4 col-lg-4 col-md-4">
+                          <div class="thumbnail">
+                              <img src="http://placehold.it/320x150" alt="">
+                              <div class="caption">
+                                  <h4 class="pull-right">$64.99</h4>
+                                  <h4><a href="#">Second Product</a>
+                                  </h4>
+                                  <p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                              </div>
+                              <div class="ratings">
+                                  <p class="pull-right">12 reviews</p>
+                                  <p>
+                                      <span class="glyphicon glyphicon-star"></span>
+                                      <span class="glyphicon glyphicon-star"></span>
+                                      <span class="glyphicon glyphicon-star"></span>
+                                      <span class="glyphicon glyphicon-star"></span>
+                                      <span class="glyphicon glyphicon-star-empty"></span>
+                                  </p>
+                              </div>
+                          </div>
+                      </div>
+
+                      <div class="col-sm-4 col-lg-4 col-md-4">
+                          <div class="thumbnail">
+                              <img src="http://placehold.it/320x150" alt="">
+                              <div class="caption">
+                                  <h4 class="pull-right">$74.99</h4>
+                                  <h4><a href="#">Third Product</a>
+                                  </h4>
+                                  <p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                              </div>
+                              <div class="ratings">
+                                  <p class="pull-right">31 reviews</p>
+                                  <p>
+                                      <span class="glyphicon glyphicon-star"></span>
+                                      <span class="glyphicon glyphicon-star"></span>
+                                      <span class="glyphicon glyphicon-star"></span>
+                                      <span class="glyphicon glyphicon-star"></span>
+                                      <span class="glyphicon glyphicon-star-empty"></span>
+                                  </p>
+                              </div>
+                          </div>
+                      </div>
+
+                      <div class="col-sm-4 col-lg-4 col-md-4">
+                          <div class="thumbnail">
+                              <img src="http://placehold.it/320x150" alt="">
+                              <div class="caption">
+                                  <h4 class="pull-right">$84.99</h4>
+                                  <h4><a href="#">Fourth Product</a>
+                                  </h4>
+                                  <p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                              </div>
+                              <div class="ratings">
+                                  <p class="pull-right">6 reviews</p>
+                                  <p>
+                                      <span class="glyphicon glyphicon-star"></span>
+                                      <span class="glyphicon glyphicon-star"></span>
+                                      <span class="glyphicon glyphicon-star"></span>
+                                      <span class="glyphicon glyphicon-star-empty"></span>
+                                      <span class="glyphicon glyphicon-star-empty"></span>
+                                  </p>
+                              </div>
+                          </div>
+                      </div>
+
+                      <div class="col-sm-4 col-lg-4 col-md-4">
+                          <div class="thumbnail">
+                              <img src="http://placehold.it/320x150" alt="">
+                              <div class="caption">
+                                  <h4 class="pull-right">$94.99</h4>
+                                  <h4><a href="#">Fifth Product</a>
+                                  </h4>
+                                  <p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                              </div>
+                              <div class="ratings">
+                                  <p class="pull-right">18 reviews</p>
+                                  <p>
+                                      <span class="glyphicon glyphicon-star"></span>
+                                      <span class="glyphicon glyphicon-star"></span>
+                                      <span class="glyphicon glyphicon-star"></span>
+                                      <span class="glyphicon glyphicon-star"></span>
+                                      <span class="glyphicon glyphicon-star-empty"></span>
+                                  </p>
+                              </div>
+                          </div>
+                      </div>
+
+                      <div class="col-sm-4 col-lg-4 col-md-4">
+                          <h4><a href="#">Like this template?</a>
+                          </h4>
+                          <p>If you like this template, then check out <a target="_blank" href="http://maxoffsky.com/code-blog/laravel-shop-tutorial-1-building-a-review-system/">this tutorial</a> on how to build a working review system for your online store!</p>
+                          <a class="btn btn-primary" target="_blank" href="http://maxoffsky.com/code-blog/laravel-shop-tutorial-1-building-a-review-system/">View Tutorial</a>
+                      </div>
+
+                  </div>
+
+              </div>
+
+          </div>
+
+      </div>
+      <!-- /.container -->
+
+      <div class="container">
+
+          <hr>
+
+          <!-- Footer -->
+          <footer>
+              <div class="row">
+                  <div class="col-lg-12">
+                      <p>Copyright &copy; Your Website 2014</p>
+                  </div>
+              </div>
+          </footer>
+
+      </div>
+      <!-- /.container -->
+
+      <!-- jQuery -->
+      <script src="js/jquery.js"></script>
+
+      <!-- Bootstrap Core JavaScript -->
+      <script src="js/bootstrap.min.js"></script>
+
   </body>
 </html>
