37e521347e7a7cb8143c432bd74319a3971caff8
Robzzn <robin2507@hotmail.de>
2017-06-02 16:34:21 +0200

    ChangeRegisterUser

--- register.php
+++ register.php
@@ -10,54 +10,56 @@ and open the template in the editor.
         <title>Registrierung</title>
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-<!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
     </head>
     <body>
         <?php
         include "dbconn.php";
         if (isset($_POST['Inputvname'])) {
-        $anrede = $_POST['InputAnrede'];
-        $vname = $_POST['Inputvname'];
-        $nname = $_POST['Inputnname'];
-        $strasse = $_POST['Inputstrasse'];
-        $plz = $_POST['Inputplz'];
-        $ort = $_POST['Inputort'];
-        $mail = $_POST['Inputmail'];
-        $username = $_POST['Inputbenutzer'];
-        $password = $_POST['Inputpass'];
-        $passwordwdh = $_POST['Inputpass2'];
+            $anrede = $_POST['InputAnrede'];
+            $vname = $_POST['Inputvname'];
+            $nname = $_POST['Inputnname'];
+            $strasse = $_POST['Inputstrasse'];
+            $plz = $_POST['Inputplz'];
+            $ort = $_POST['Inputort'];
+            $mail = $_POST['Inputmail'];
+            $username = $_POST['Inputbenutzer'];
+            $password = $_POST['Inputpass'];
+            $passwordwdh = $_POST['Inputpass2'];
+            
+            if(!preg_match('/^([A-ZÖÜÄ][a-züäö]+[ ]?){1,2}$/',$vname)){
+		$userError = "Bitte gültigen Namen eintragen.";
+		$errorOccurred = 1;
+	}
+            
+            
+            
 
-        $password = md5($password);
+            $password = md5($password);
 
-        $sql = "SELECT id FROM users WHERE username = '$username'";
+            $sql = "SELECT id FROM users WHERE username = '$username'";
 
-        $result = $dbconn->query($sql);
-        $menge = $result->num_rows;
+            $result = $dbconn->query($sql);
+            $menge = $result->num_rows;
 
-        if($menge == 0)
-        {
-            $eintrag = "INSERT INTO users (anrede, vorname, nachname, strasse, plz, ort, mail, username, passwd) VALUES ('$anrede', '$vname', '$nname', '$strasse', '$plz', '$ort', '$mail', '$username', '$password')";
+            if ($menge == 0) {
+                $eintrag = "INSERT INTO users (anrede, vorname, nachname, strasse, plz, ort, mail, username, passwd) VALUES ('$anrede', '$vname', '$nname', '$strasse', '$plz', '$ort', '$mail', '$username', '$password')";
 
-            $eintragen = $dbconn->query($eintrag);
+                $eintragen = $dbconn->query($eintrag);
 
-            if($eintragen == true)
-            {
-            header("Location: login.php");
-            exit;
+                if ($eintragen == true) {
+                    header("Location: login.php");
+                    exit;
+                } else {
+                    header("Location: register.php");
+                    exit;
+                }
+            } else {
+                header("Location: register.php");
+                exit;
             }
-            else{
-            header("Location: register.php");
-            exit;
-            }
-        }
-        else
-        {
-        header("Location: register.php");
-        exit;
-        }
-
         }
         ?>
         <form class='col-md-8' action='register.php' method='Post'>
@@ -70,39 +72,39 @@ and open the template in the editor.
             </div>
             <div class='form-group'>
                 Vorname
-                <input type='text' class='form-control' name='Inputvname' placeholder='Vorname'>
+                <input type='text' class='form-control' name='Inputvname' placeholder='Vorname' required="true">
             </div>
             <div class='form-group'>
                 Nachname
-                <input type='text' class='form-control' name='Inputnname' placeholder='Nachname'>
+                <input type='text' class='form-control' name='Inputnname' placeholder='Nachname' required="true">
             </div>
             <div class='form-group'>
                 Strasse
-                <input type='text' class='form-control' name='Inputstrasse' placeholder='Straße'>
+                <input type='text' class='form-control' name='Inputstrasse' placeholder='Straße' required="true">
             </div>
             <div class='form-group'>
                 PLZ
-                <input type='text' class='form-control' name='Inputplz' placeholder='PLZ'>
+                <input type='text' class='form-control' name='Inputplz' placeholder='PLZ' required="true">
             </div>
             <div class='form-group'>
                 Ort
-                <input type='text' class='form-control' name='Inputort' placeholder='Ort'>
+                <input type='text' class='form-control' name='Inputort' placeholder='Ort' required="true">
             </div>
             <div class='form-group'>
                 Email Adresse
-                <input type='email' class='form-control' name='Inputmail' placeholder='Mail'>
+                <input type='email' class='form-control' name='Inputmail' placeholder='Mail' required="true">
             </div>
             <div class='form-group'>
                 Benutzername
-                <input type='text' class='form-control' name='Inputbenutzer' placeholder='Benutzer'>
+                <input type='text' class='form-control' name='Inputbenutzer' placeholder='Benutzer' required="true">
             </div>
             <div class='form-group'>
                 Passwort
-                <input type='password' class='form-control' name='Inputpass' placeholder='Passwort'>
+                <input type='password' class='form-control' name='Inputpass' placeholder='Passwort' required="true">
             </div>
             <div class='form-group'>
                 Passwort wiederholen
-                <input type='password' class='form-control' name='Inputpass2' placeholder='Passwort'>
+                <input type='password' class='form-control' name='Inputpass2' placeholder='Passwort' required="true">
             </div>
             <button type='submit' class='btn btn-default'>Registrieren</button>
         </form>
